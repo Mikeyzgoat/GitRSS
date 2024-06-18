72a0201b3a86ab265e069ad871c04fe0cd246b6f
Mikeyzgoat <tadisai091@gmail.com>
2024-06-18 20:53:34 +0530

    removed js related files

--- constants.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-export const DISCORD_TOKEN = process.env.API_KEY;
-export const URL = process.env.GITHUB_RSS_FEED;
-
--- parser/rss_parser.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-import { 
-    URL,
-    DISCORD_TOKEN
- } from '../constants';
-
-console.log(URL)
